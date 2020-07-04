17d3afd84062950392cc4d4b2f501165abf73514
HieuTran <63003697+Hieupro789987@users.noreply.github.com>
2020-07-04 08:37:39 +0700

    Create PageHoaDon.php

---
+++ ĐE2_LHL/PageHoaDon.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hóa Đơn</title>
+</head>
+<style>
+    .exam1,{
+            margin:50px auto;
+            padding:10px;
+            text-align:center;
+            border:2px solid black;
+        }
+        table, th, td ,a{
+        border: 1px solid black;
+        text-decoration:none;
+        text-align:center;
+        }
+ </style>   
+<body>
+<h1 class="" style="text-align:center;">Bảng Hóa Đơn</h1>
+
+<div class="exam1">
+        <table style="width:100%">
+            <tr>
+                <th>Mã HĐ</th>
+                <th>Mã HH</th>
+                <th>Tên HH</th>
+                <th>Ngày Lập </th>
+                <th>Mã KH</th>
+                <th>Giá Tiền</th>
+            </tr>
+         
+          
+            <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+            </tr>
+          
+            <div style="border:1px solid black;width:300px;margin-bottom:30px;">
+                <h5>ToTal :</h5>
+                <span></span>
+            <div>
+       
+        </table>  
+    </div>
+</body>
+</html>
