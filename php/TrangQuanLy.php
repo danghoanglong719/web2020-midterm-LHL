312ce426f542f0b2d1ff9c951a932359200c155e
HieuTran <63003697+Hieupro789987@users.noreply.github.com>
2020-07-04 10:58:19 +0700

    Done

--- php/TrangQuanLy.php
+++ php/TrangQuanLy.php
@@ -29,7 +29,7 @@
  </style>   
 <body>
 <?php
-        include_once './ketnoi.php';
+        include_once 'ketnoi.php';
         $result = mysqli_query($conn , "SELECT * FROM hanghoa");
      
 
@@ -68,7 +68,7 @@
                             <td> <img src="../Hinh/<?=$row['Hinh']?>" class="img"/> </td>                  
                              <td> <a href="">Thêm</a></td>
                              <td> <a href="./TrangQuanLy.php?delete=<?=$row['MaHH'] ?>">Xóa</a></td>
-                             <td> <a href="">Sửa</a></td>
+                             <td> <a href="./TrangQuanLy.php?edit=<?=$row['MaHH'] ?>">Sửa</a></td>
                          </div>
                 <?php } ?>
     
@@ -82,11 +82,55 @@ if(isset($_GET['delete'])){
     if($result==true)
     {
         echo "thanh cong";
+        header("location:TrangQuanLy.php");
     }
     else {
         echo "that bai";}
 }
+?>       
+<?php
+include_once 'ketnoi.php';
+
+if(isset($_POST['sub']) && isset($_GET['edit']))
+{
+    $edit = $_POST['change'];
+    $Ma = $_POST['Ma'];
+    $price = $_POST['price'];
+    $img = $_POST['img'];
+    $id= $_GET['edit'];
+    $result = mysqli_query($conn , "UPDATE   hanghoa  SET TenHH ='$edit' , MaLoai = '$Ma' , DonGia = '$price', Hinh = '$img' where  MaHH ='$id'");
+
+    if($result==true)
+    {
+        echo "thanh cong";
+        header("location:TrangQuanLy.php");
+    }
+    else {
+        echo "that bai";}
+}
+
+
 ?>
+<div>
+        <form action="" method="POST" style="margin-top:20px;">
+        <label for="">MaLoai</label>
+        <input type="text" name="Ma" value=""required>
+        <br>
+        <label for="">TênHH</label>
+        <input type="text" name="change" value=""required>
+        <br>
+        <label for="">ĐơnGia</label>
+        <input type="text" name="price" value=""required>
+        <br>
+        <label for="">HinhAnh</label>
+        <input type="text" name="img" value="">
+        <br>
+        <input type="submit" name="sub">
+
+</form>
+</div>
+
+        
 
 </div>
 </body>
