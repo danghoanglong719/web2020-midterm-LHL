79cad9360c4bf94ea9bdc1f38a3889e9e32015a2
HieuTran <63003697+Hieupro789987@users.noreply.github.com>
2020-07-04 10:45:39 +0700

    button Xoa

--- php/TrangQuanLy.php
+++ php/TrangQuanLy.php
@@ -43,20 +43,6 @@
         <h1 class="" style="text-align:center;">Bảng Quản Lý</h1>
 
 <div class="exam1">
-<<<<<<< HEAD:ĐE2_LHL/TrangQuanLy.php
-=======
-    <!-- Tab links -->
-    <div class="tab">
-        <button class="tablinks" onclick="openCity(event, 'hanghoa')">HÀNG HÓA</button>
-        <button class="tablinks" onclick="openCity(event, 'loai')">LOẠI</button>
-        <button class="tablinks" onclick="openCity(event, 'khachhang')">KHÁCH HÀNG</button>
-        <button class="tablinks" onclick="openCity(event, 'hoadon')">HÓA ĐƠN</button>
-        <button class="tablinks" onclick="openCity(event, 'chitiethd')">CHI TIẾT HÓA ĐƠN</button>
-    </div>
-
-    <!-- Tab content -->
-    <div id="hanghoa" class="tabcontent">
->>>>>>> 414e7da16f09e4ece3cf4d4f80389c27508c41b1:php/TrangQuanLy.php
         <table style="width:100%">
             <tr>
                 <th>Mã Sản Phẩm</th>
@@ -90,7 +76,6 @@
     </div>
     <?php
 
-<<<<<<< HEAD:ĐE2_LHL/TrangQuanLy.php
 if(isset($_GET['delete'])){
     $product_id = $_GET['delete'];
     $result = mysqli_query($conn , "DELETE FROM `hanghoa` WHERE MaHH='$product_id'");
@@ -103,100 +88,6 @@ if(isset($_GET['delete'])){
 }
 ?>
 
-=======
-    <div id="loai" class="tabcontent">
-        <table style="width:100%">
-            <tr>
-                <th>Mã Loại</th>
-                <th>Tên Loại</th>
-                <th>Mô tả</th>
-                <th>THÊM</th>
-                <th>XÓA</th>
-                <th>SỬA</th>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <th><a href=""> </a></th>
-                <th><a href=""> </a></th>
-                <th><a href=""> </a></th>
-            </tr>
-            <tr></tr>
-        </table>
-    </div>
-
-    <div id="khachhang" class="tabcontent">
-        <table style="width:100%">
-            <tr>
-                <th>Mã KH</th>
-                <th>Họ Tên</th>
-                <th>Điện thoại</th>
-                <th>Email</th>
-                <th>THÊM</th>
-                <th>XÓA</th>
-                <th>SỬA</th>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <th><a href=""> </a></th>
-                <th><a href=""> </a></th>
-                <th><a href=""> </a></th>
-            </tr>
-            <tr></tr>
-        </table>
-    </div>
-    <div id="hoadon" class="tabcontent">
-        <table style="width:100%">
-            <tr>
-                <th>Mã Hóa Đơn</th>
-                <th>Ngày Lập</th>
-                <th>Mã Khách Hàng</th>
-                <th>Tổng Tiền</th>
-                <th>THÊM</th>
-                <th>XÓA</th>
-                <th>SỬA</th>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <th><a href=""> </a></th>
-                <th><a href=""> </a></th>
-                <th><a href=""> </a></th>
-            </tr>
-            <tr></tr>
-        </table>
-    </div>
-
-    <div id="chitiethd" class="tabcontent">
-        <table style="width:100%">
-            <tr>
-                <th>Mã CTHD</th>
-                <th>Mã Hóa Đơn</th>
-                <th>Mã Hàng Hóa</th>
-                <th>Số Lượng</th>
-                <th>THÊM</th>
-                <th>XÓA</th>
-                <th>SỬA</th>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <th><a href=""> </a></th>
-                <th><a href=""> </a></th>
-                <th><a href=""> </a></th>
-            </tr>
-            <tr></tr>
-        </table>
-    </div>
->>>>>>> 414e7da16f09e4ece3cf4d4f80389c27508c41b1:php/TrangQuanLy.php
 </div>
 </body>
 </html>
