9f9f9a0b0ae70c2b096236f7e7604584eb2cc378
HieuTran <63003697+Hieupro789987@users.noreply.github.com>
2020-07-04 08:06:40 +0700

    Create TrangQuanLy.php

---
+++ ĐE2_LHL/TrangQuanLy.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quản Lý</title>
+</head>
+<style>
+    .exam1,{
+            margin:50px auto;
+            padding:10px;
+            text-align:center;
+            border:2px solid black;
+        }
+        table, th, td ,a{
+        border: 1px solid black;
+        text-decoration:none;
+        }
+ </style>   
+<body>
+        <h1 class="" style="text-align:center;">Bảng Quản Lý</h1>
+
+<div class="exam1">
+        <table style="width:100%">
+            <tr>
+                <th>LOẠI</th>
+                <th>HÀNG HÓA</th>
+                <th>KHÁCH HÀNG</th>
+                <th>HÓA ĐƠN</th>
+                <th>CHI TIẾT HÓA ĐƠN</th>
+                <th>THÊM</th>
+                <th>XÓA</th>
+                <th>SỬA</th>
+            </tr>
+         
+         
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <th><a href=""> </a></th>
+                <th><a href=""> </a></th>
+                <th><a href=""> </a></th>
+            </tr>
+    
+       
+        </table>  
+    </div>
+</body>
+</html>
