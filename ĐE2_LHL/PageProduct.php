f9c51ba7b56e2fe75d8b0bc4c7bc2ec575ff5f6a
HieuTran <63003697+Hieupro789987@users.noreply.github.com>
2020-07-04 08:37:12 +0700

    Create PageProduct.php

---
+++ ĐE2_LHL/PageProduct.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sản Phẩm</title>
+</head>
+<style>
+    .exam1,{
+            margin:50px auto;
+            padding:10px;
+            text-align:center;
+            border:2px solid black;
+        }
+        table, th, td ,a{
+        border: 1px solid black;
+        text-decoration:none;
+        text-align:center;
+        }
+ </style>   
+<body>
+        <h1 class="" style="text-align:center;">Bảng Sản Phẩm</h1>
+<div class="exam1">
+        <table style="width:100%">
+            <tr>
+                <th>Mã HH</th>
+                <th>Tên HH</th>
+                <th>Đơn Giá</th>
+                <th>Hình</th>
+                <th>check</th>
+                <th>Số Lượng</th>
+                <th>Mua HH</th>
+            </tr>
+         
+         
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td><input type="checkbox" name="check" value=""> </td>
+                <td><input type="number" name="numb" value=""> </td>
+                <td><a href="">Mua</a></td>
+            </tr>
+    
+       
+        </table>  
+    </div>
+
+</body>
+</html>
